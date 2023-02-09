db52cfeca4dd1217247d3e5fe146d26cc3eef34c
billy <Billy@mask.io>
2023-02-09 17:21:26 +0800

    fix: background flare

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app";
 import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { mainnet, polygon, optimism, arbitrum, goerli } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { ToastProvider } from "react-toast-notifications";
 
@@ -34,6 +33,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <ToastProvider>
+          <div className="web3bio-cover"></div>
           <Component {...pageProps} />
         </ToastProvider>
       </RainbowKitProvider>
