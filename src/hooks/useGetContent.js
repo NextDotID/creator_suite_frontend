9928d11af25949a7364c902054b4639ed1d2b438
billy <Billy@mask.io>
2023-03-27 21:15:36 +0800

    fix: useContent

--- src/hooks/useGetContent.js
+++ src/hooks/useGetContent.js
@@ -4,9 +4,12 @@ export function useGetContent(publicKey, id) {
   return useSWR(
     `useGetContent_${publicKey}_${id}`,
     async () => {
-      const res = await fetch(
-        `http://localhost:8000/api/v1/get-content?public_key=${publicKey}&content_id=${id}`
-      );
+      const res = await fetch(`http://localhost:8000/api/v1/get-content`, {
+        body: JSON.stringify({
+          public_key: publicKey,
+          content_id: id,
+        }),
+      });
       if (res) {
         const _res = await res.json();
         console.log(_res, "ecriptedData");
